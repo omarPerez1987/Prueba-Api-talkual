907e7db6a4578e872a98914d39f1898a46b2b69d
omarPerez1987 <omar.perez.dev@gmail.com>
2024-03-19 15:05:35 +0100

    feat: Agregar funciones updateOrder y createOrder al servicio de order (Estas funciones se importan en el controlador de la función donate para su uso en el procesamiento de donaciones de pedidos.)

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,6 +4,8 @@
 
 import { factories } from '@strapi/strapi'
 import { isValidPostalCode } from '../services/coverageService'
+import { updateOrder, createOrder } from '../services/order';
+
 
 export default factories.createCoreController('api::order.order', ({ strapi }) => ({
   async donate (ctx): Promise<any> {
@@ -12,10 +14,23 @@ export default factories.createCoreController('api::order.order', ({ strapi }) =
       const authenticatedUser = ctx.state.user;
       const { order_meta } = ctx.request.body;
       const order = await strapi.service('api::order.order').findOne(sanitizedQueryParams, { populate: ['order_items', 'order_meta'] });
+      
 
-      /***** Rest of the code here *****/
+      if (!isValidPostalCode(order_meta.shipping_postcode)) {
+        return ctx.throw(400, 'Código postal inválido');
+      }
 
-      return {order, order_meta, authenticatedUser};
+      const updatedOrder = await updateOrder(order.id, { status: 'cancelled' });
+
+      const newOrderData = {
+        status: 'processing',
+        type: 'donation',
+        user: order.id,
+      };
+      const newOrder = await createOrder(newOrderData);
+
+      return {updatedOrder, newOrder}
+      // return {order, updatedOrder, newOrder, order_meta, authenticatedUser};
     } catch (error) {
       console.error('Error exporting orders', error);
       return ctx.status = 500;
--- src/api/order/services/order.ts
+++ src/api/order/services/order.ts
@@ -4,4 +4,29 @@
 
 import { factories } from '@strapi/strapi';
 
+export const updateOrder = async (orderId: number, updatedData: any): Promise<any> => {
+    try {
+      const updatedOrder = await strapi.entityService.update('api::order.order', orderId, {
+        data: updatedData,
+      });
+      console.log(updatedOrder)
+      return updatedOrder;
+    } catch (error) {
+      console.error('Error al actualizar el pedido:', error);
+      throw error;
+    }
+  };
+  
+export const createOrder = async (newOrderData: any): Promise<any> => {
+    try {
+      const newOrder = await strapi.entityService.create('api::order.order', {
+        data: newOrderData,
+      });
+      return newOrder;
+    } catch (error) {
+      console.error('Error al crear el nuevo pedido:', error);
+      throw error;
+    }
+  };
+
 export default factories.createCoreService('api::order.order');
